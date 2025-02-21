57ef58f5767a2d228fa8bf752fffd7aa864ef94c
jatin-rajput563 <jkt3298@gmail.com>
2025-02-21 11:17:56 +0530

    scetion-5-complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,23 @@ const navbarbtn = () => {
     document.getElementById("rotats2").classList.toggle("rotate-43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+$('.slider-card').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".left-arrow",
+    nextArrow: ".right-arrow",
+    responsive: [
+        {
+            breakpoint: 856,
+            settings: {
+                prevArrow: false,
+                nextArrow: false,
+                dots: true,
+            }
+        },
+    ]
+});
